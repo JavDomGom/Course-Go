bf0a6564189d4e2a802c6c960a26b7315fb0a7fa
JavierDominguezGomez <jdomgo3@gmail.com>
2020-11-20 21:54:10 +0100

    Channels.

--- 10_Channels/09_Context/main.go
+++ 10_Channels/09_Context/main.go
@@ -7,21 +7,18 @@ import (
 
 func main() {
 	ctx := context.Background()
-
 	fmt.Println("context:\t\t", ctx)
 	fmt.Println("error de context:\t", ctx.Err())
 	fmt.Printf("tipo de context:\t%T\n", ctx)
 	fmt.Println("----------")
 
 	ctx, _ = context.WithCancel(ctx)
-
 	fmt.Println("context:\t\t", ctx)
 	fmt.Println("error de context:\t", ctx.Err())
 	fmt.Printf("tipo de context:\t%T\n", ctx)
 	fmt.Println("----------")
 
 	ctx, cancel := context.WithCancel(ctx)
-
 	fmt.Println("context:\t\t", ctx)
 	fmt.Println("error de context:\t", ctx.Err())
 	fmt.Printf("tipo de context:\t%T\n", ctx)
